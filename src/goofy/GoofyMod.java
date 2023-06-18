aa9e68459de47c5744041fd2d826171d84799b9d
Lime712 <122940036+Lime712@users.noreply.github.com>
2023-06-18 23:47:22 +1200

    Rename Files

---
+++ src/goofy/GoofyMod.java
@@ -0,0 +1,24 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+        
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
